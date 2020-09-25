733ea964ac2f9bfa884d39df676c0630fb6a8e61
Adam Alcorn <adamalcorn@Adams-MacBook-Pro.local>
2020-09-25 13:00:44 +0100

    Homework 1

---
+++ src/homework.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+class Time {
+    public static void delay( int msec ) {
+        // Pause thread for specified number of milliseconds
+        try {
+            Thread.sleep( msec );
+        } catch( InterruptedException e ) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+public class homework {
+ public static int n = 0;
+ public static int k =10;
+    public static void main (String[] args) {
+
+        Thread p1 = new P1 () ;
+        Thread p2 = new P2 () ;
+        Thread display = new Display ();
+
+        p1.start ();
+        p2.start ();
+        display.start();
+    }
+}
+
+class P1 extends Thread {
+
+    int temp = 0;
+
+    public void run ()  {
+        Random rnd = new Random();
+        for(int i= 0; i < homework.k ; i++){
+        // Time.delay(rnd.nextInt(20));
+            temp = homework.n;
+            homework.n = temp + 1;
+            System.out.println("run 1 n = " + homework.n);
+        }
+        System.out.println ("P1 finished");
+    }
+}
+
+class P2 extends Thread {
+
+    int temp = 0;
+
+    public void run ()  {
+        Random rnd = new Random();
+        for(int i= 0; i < homework.k ; i++) {
+          // Time.delay(rnd.nextInt(20));
+            temp = homework.n;
+            homework.n = temp - 1;
+            System.out.println("run 2 n = " + homework.n);
+        }
+        System.out.println ("P2 finished");
+    }
+}
+
+
+class Display extends Thread {
+    public void run () {
+        Time.delay(500);
+        System.out.println ("N = " + homework.n);
+    }
+}
